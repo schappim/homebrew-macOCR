3050c1a356fd8dc977818060f14347f5dd792df4
Marcus S <marcus@schappi.com>
2021-11-10 14:17:38 +1100

    Update ocr.rb

--- ocr.rb
+++ ocr.rb
@@ -1,7 +1,7 @@
 class Ocr < Formula
   desc "Get any text on your screen into your clipboard."
   homepage "https://github.com/schappim/macOCR"
-  url "https://files.littlebird.com.au/ocr2.zip"
+  url "https://files.littlebird.com.au/macOCR-0.0.2.tar.gz"
   sha256 "bd1d955212cde93f8bfb657dd56a0ed3d724a30980e19172bc8832b5cdb279b8"
 
   bottle :unneeded
